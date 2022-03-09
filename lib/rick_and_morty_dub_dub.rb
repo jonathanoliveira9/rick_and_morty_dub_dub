3677a41d8f6fa0b613c60b39f5162df99af62753
Jonathan de Oliveira Silva <jonathan.silva.impulso@yapay.com.br>
2022-03-08 23:44:47 -0300

    first commit

---
+++ lib/rick_and_morty_dub_dub.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rick_and_morty_dub_dub/version"
+
+module RickAndMortyDubDub
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rick_and_morty_dub_dub/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RickAndMortyDubDub
+  VERSION = "0.1.0"
+end
